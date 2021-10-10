31093428c067902c85be42817f8d86c2196dc289
Partyk Kuchta <kuchtavr6@gmail.com>
2021-10-10 11:58:14 +0100

    version 0.1.1

---
+++ index.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html>
+
+<head lang="en">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Patryk Kuchta</title>
+    <link rel="stylesheet" href="stylesheets/reset.css" type="text/css" />
+    <link rel="stylesheet" href="stylesheets/style.css" type="text/css" />
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Gemunu+Libre:wght@200;300&family=Mulish:wght@300;500&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
+    <div class="container-fluid p-0 m-0 w-100 d-inline">
+        <div class="row p-0 m-0" id="intro">
+            <div class="col-md-5">
+            </div>
+            <div class="col-md-5" id="intro_text">
+                <h1>Hello! My name is <b>Patryk Kuchta</b>. I'm a Computer Science Student at Queen Mary University in
+                    London. I'm looking forward to becoming a <b>Software Engineer</b>.</h1>
+                <p>
+                    Growing up in the era of rapid digitalization, by the time I was in high school, I was sure where I
+                    was heading for my career, and it was computer science. Why? Because computers are my hobby and I
+                    love solving logical-mathematical problems, furthermore there are plenty of ways to apply IT to
+                    regular Engineering, which always was something that I was interested in. Getting a place at a very
+                    reputable Queen Mary University in London and a job at Queen Mary University as a Demonstrator
+                    (Teaching Assistant)
+                    put me on the right track to achieve success in my life.
+                </p>
+            </div>
+            <div class="col-md-2">
+            </div>
+        </div>
+        <div class="row p-0 m-0" id="education">
+            <div class="col-md-2">
+            </div>
+            <div class="col-md-3" id="edu_text">
+                <h1>
+                    <b>"An investment in knowledge pays the best interest."</b> <i>Benjamin Franklin</i>
+                </h1>
+            </div>
+            <div class="col-md-5" id="edu_text">
+                <p style="text-align:left;">
+                    Second year student at Queen Mary University, London
+                    <span style="float:right;">
+                        <b>Since September 2020</b>
+                    </span><br>
+                    Course: (BSc) Computer Science <br>
+                    • First year’s average mark at 89% <br>
+                    • 1st in all eight modules
+                </p>
+                <hr>
+                <p style="text-align:left;">
+                    Jan Zamoyski High School, Warsaw
+                    <span style="float:right;">
+                        <b>2017 - 2020</b>
+                    </span><br>
+                    • Class with extended Mathematics, IT and Physics
+                </p>
+            </div>
+            <div class="col-md-2">
+            </div>
+        </div>
+        <div class="row p-0 m-0" id="experience">
+            <div class="col-md-1"></div>
+            <div class="col-md-10">
+                <h1 id="exp_head">
+                    My professional experience
+                </h1>
+                <p id="exp_text">
+                    Although I am only 20 years old, I have already worked in 4 different companies. This of course gave
+                    me a lot of insight into how companies work, both those big and startups. My early jobs
+                    centred around hospitality and retail, which gave me knowledge about problems that could be solved
+                    using IT in those areas. Lately, I'm mostly involved in positions about teaching and supporting
+                    students, which are stimulating my skills that are important in IT workplaces, for example, the
+                    ability to explain concepts to a 'non-IT' crowd, and leading a team.
+                </p>
+                <div class="row pt-4 m-0">
+                    <div class="col-md-6 block p-2">
+                        <div class="binner">
+                            <p style="text-align:left;">
+                                <b>Demonstrator at Queen Mary University in London</b>
+                                <span style="float:right;">
+                                    <b>Since September 2021</b>
+                                </span><br>
+                                <hr>
+                                <b>
+                                    • Improved my interpersonal skills<br>
+                                    • Gained further professionalism in a remote working environment<br>
+                                    • Gained knowledge in explaining IT concepts in a professional manner<br>
+                                    • Tailored resources for individual needs
+                                </b>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 block p-2">
+                        <div class="binner">
+                            <p style="text-align:left;">
+                                <b>Customer Assistant at Morrisons in London</b>
+                                <span style="float:right;">
+                                    <b>Since September 2020</b>
+                                </span><br>
+                                <hr>
+                                <b>
+                                    • Provided friendly and efficient customer service<br>
+                                    • Prepared and packed online shopping in a timely manner<br>
+                                    • Had a daily duty of thoroughly checking the freshness of products
+                                    • Worked on minimizing losses and food waste
+                                </b>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row p-0 m-0">
+                    <div class="col-md-3"></div>
+                    <div class="col-md-6 block p-2">
+                        <div class="binner">
+                            <p style="text-align:left;">
+                                <b>Coding Tutor at Kodland in London</b>
+                                <span style="float:right;">
+                                    <b>May - August 2021</b>
+                                </span><br>
+                                <hr>
+                                <b>
+                                    • Improved my presentational skills<br>
+                                    • Gained experience in a remote working environment<br>
+                                    • Gained skills in explaining programming principles<br>
+                                </b>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-3"></div>
+                </div>
+                <div class="row pb-5 m-0">
+                    <div class="col-md-3"></div>
+                    <div class="col-md-6 block p-2">
+                        <div class="binner">
+                            <p style="text-align:left;">
+                                <b>Customer Assistant at McDonald’s Restaurant, Warsaw</b>
+                                <span style="float:right;">
+                                    <b>June – August 2018</b>
+                                </span><br>
+                                <hr>
+                                <b>
+                                    • Provided friendly and efficient customer service<br>
+                                    • Provided recommendations and information about food and drinks<br>
+                                    • Recorded customer’s orders and took payments<br>
+                                    • Completed, prepared, and served orders
+                                </b>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-3"></div>
+                </div>
+            </div>
+            <div class="col-md-1"></div>
+        </div>
+    </div>
+</body>
+
+</html>
